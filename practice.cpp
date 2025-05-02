53c065f934c7df34b2aa94beca895adf9657ac89
Subhi2911 <msubhasmita29@gmail.com>
2025-05-02 18:48:43 +0530

    Initial commit

---
+++ practice.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int i;
+    int k;
+    
+    
+    
+    for (int i=1;i<=5;i++){
+        for (int k=1;i<=4;k++){
+                cout<<' ';
+            }
+        for (int j=1; j<=i;j++){
+            cout<<"*";
+                
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
+
